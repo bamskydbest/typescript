c6dda6ca795d8cbf760698c8c4a14b452ba93b28
User <user@Mahmoud-MacBook-Pro.local>
2025-03-24 18:07:26 +0100

    Module 25 Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import FirstProject from "./components/FirstProject";
 
 function App() {
+  document.title = "Local State";
   return (
     <>
       <h1>Storing Items in Local State</h1>
+      <FirstProject />
     </>
   );
 }
